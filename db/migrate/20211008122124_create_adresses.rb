c5dcefc8205add8334722e62bb22d8644eefe633
FSauna <yfuta0630@gmail.com>
2021-10-08 12:45:22 +0000

    [Add] deviseインストール

---
+++ db/migrate/20211008122124_create_adresses.rb
@@ -0,0 +1,14 @@
+class CreateAdresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :adresses do |t|
+      t.integer :customer_id
+      t.string :name
+      t.string :postal_code
+      t.string :address
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
