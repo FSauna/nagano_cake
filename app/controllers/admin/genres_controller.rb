46f0cea7647512fbec6a5048e8611a35cee67c7b
FSauna <yfuta0630@gmail.com>
2021-10-09 13:04:21 +0000

    [Add] ジャンル一覧・追加画面たたき台

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,16 @@
 class Admin::GenresController < ApplicationController
+  
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
+  
   def create
   end
+  
   def edit
   end
+  
   def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
       passwords:     'customers/passwords',
       registrations: 'customers/registrations'
     }
+    
+  namespace :admin do
+    resources :genres, only:[:index, :create, :edeit, :update]
+  end
   
 
 end
