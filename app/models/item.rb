3087a825bf8607feb8920ac07755d3dc2276c8f2
FSauna <yfuta0630@gmail.com>
2021-10-10 05:40:55 +0000

    [Add] 商品詳細画面たたき台作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   belongs_to :genre
   attachment :image
   
+  def tax_price
+    price * 1.1
+  end
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6ea88054b03bed1fc88aa4eeee36da757db8f916a636a872d5bf46144d0739ed49d66b7afc35b2b146c0de0eecb8b335562889c57168e6596ed67bb09d69238d'
