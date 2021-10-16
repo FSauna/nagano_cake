aede8269a22e15061d7603087cba9a6c47bf738d
FSauna <yfuta0630@gmail.com>
2021-10-16 13:18:07 +0000

    [Add] 会員側ログイン画面

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -24,4 +24,14 @@ class Customers::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  private
+    def after_sign_in_path_for(resource)
+      if resource.is_deleted == false
+        sign_out resource
+        root_path
+      else
+        root_path
+      end
+    end
 end
