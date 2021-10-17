f5d92a5a88ca68830a56409d2a9217a566a195e7
FSauna <yfuta0630@gmail.com>
2021-10-17 13:50:14 +0000

    [Add] 会員側登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,15 +5,24 @@ class Public::CustomersController < ApplicationController
   end
   
   def edit
+    @cutomer = current_customer
   end
   
   def update
+    @cutomer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path(@customer)
   end
   
   def unsubscribe
   end
   
   def withdraw
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
   end
   
 end
