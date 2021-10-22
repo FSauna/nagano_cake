cac1b9e74ab1d3f7c719fd3d25583a89dc52581b
FSauna <yfuta0630@gmail.com>
2021-10-22 14:18:08 +0000

    [Fix] AWS移行とか

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,15 +6,44 @@ class Public::OrdersController < ApplicationController
   
   def confirm
     @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
+    @cart_items = current_customer.cart_items
+    @order.shipping_cost = 800
+
+    if params[:order][:select_address] == "0" #ご自身の住所
+       @order.postal_code = current_customer.postal_code
+       @order.address = current_customer.address
+       @order.name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:select_address] == "1" #登録済み住所
+       @address = Address.find(params[:order][:address_id])
+       @order.postal_code = @address.postal_code
+       @order.address = @address.address
+       @order.name = @address.name
+
+    elsif params[:order][:select_address] == "2" #新しいお届け先
+       @order.postal_code = params[:order][:postal_code]
+       @order.address = params[:order][:address]
+       @order.name = params[:order][:name]
+    end
   end
   
   def thanks
   end
   
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    current_customer.cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.order_id =  @order.id
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.amount = cart_item.amount
+      @order_detail.price = cart_item.subtotal
+      @order_detail.save
+      end
+      current_customer.cart_items.destroy_all
+      redirect_to orders_complete_path
   end
   
   def index
